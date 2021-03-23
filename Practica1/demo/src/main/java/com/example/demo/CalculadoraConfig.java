880db450537833c3e6991db595228e43973bcd55
cesar123hc <71783312+cesar123hc@users.noreply.github.com>
2021-03-22 23:33:24 -0600

    Update

---
+++ Practica1/demo/src/main/java/com/example/demo/CalculadoraConfig.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.XsdSchema;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+
+public class CalculadoraConfig extends WsConfigurerAdapter{
+    @Bean
+    public XsdSchema calculadoraSchema() {
+        return new SimpleXsdSchema (new ClassPathResource("calculadora.xsd"));
+    }
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext){
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "calculadora")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema calculadoraSchema){
+        DefaultWsdl11Definition wsdl = new DefaultWsdl11Definition();
+        wsdl.setPortTypeName("calculadoraPort");
+        wsdl.setLocationUri("/ws/calculadora");
+        wsdl.setTargetNamespace("http://www.example.org/calculadora%22");
+        wsdl.setSchema(calculadoraSchema);
+        return wsdl;
+    }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
